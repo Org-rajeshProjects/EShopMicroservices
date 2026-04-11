cd57293bbf8c167a97a75cc8304eb61c0fb6b84e
rajesh-rb <rajeshbarik0707@gmail.com>
2026-04-11 20:20:32 +0530

    Order.Domain with tactical Domain-Driven Design patterns.

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification//INotification is marker interface for MediatR to identify this as an event that can be published and handled by handlers
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException(string message) : base($"Domain Exceprion: \"{message}\" throws from domain layer.") { }
+}
