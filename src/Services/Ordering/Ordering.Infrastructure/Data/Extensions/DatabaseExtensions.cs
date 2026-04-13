c7dba2514bb62b152d6b429cade2fe5393876622
rajesh-rb <rajeshbarik0707@gmail.com>
2026-04-13 19:09:22 +0530

    cofigured SQL server in Docker compose for Ordering service.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication application)
+    {
+        using var scope = application.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        context.Database.MigrateAsync().GetAwaiter().GetResult();
+    }
+}
